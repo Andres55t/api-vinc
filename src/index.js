c9e3e7fecdceb2da94c367ba98f2716bf6c009fa
Andres55t <andres@gmail.com>
2020-07-21 09:58:12 -0500

    cambio

--- src/index.js
+++ src/index.js
@@ -10,7 +10,5 @@ app.use(express.urlencoded({ extended: false }));
 //routes
 app.use(require('./routes/index'));
 
-var port_number = server.listen(process.env.PORT || 3000);
-app.listen(port_number);
-
+app.listen(3000);
 console.log("Server on port 3000");
